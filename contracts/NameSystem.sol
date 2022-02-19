e8ca9aa8de85304dc0a16676016e13d6388dbb77
anhdungle <anhdungle634@gmail.com>
2022-02-18 22:59:13 -0500

    add events

--- contracts/NameSystem.sol
+++ contracts/NameSystem.sol
@@ -6,8 +6,31 @@ contract NameSystem {
         uint256 validUntil;
     }
 
-    uint256 public pricePerByte = 100; // price per byte in wei
-    uint256 validityPeriod = 604800; // 1 week
+    event Registration(
+        address indexed newOwner,
+        address indexed previousOwner,
+        string indexed name,
+        uint256 validUntil
+    );
+
+    event Extension(
+        address indexed owner,
+        string indexed name,
+        uint256 validUntil
+    );
+
+    event Refund(
+        address indexed user,
+        uint256 amount
+    );
+
+    event Forfeiture(
+        address indexed owner,
+        string indexed name
+    );
+
+    uint256 constant public pricePerByte = 100; // price per byte in wei
+    uint256 constant public validityPeriod = 604800; // 1 week
 
     mapping(bytes32 => NameInfo) public names;
     mapping(address => mapping(bytes32 => bool)) commitments;
@@ -46,26 +69,35 @@ contract NameSystem {
         nameInfo.validUntil = block.timestamp + validityPeriod;
         /// send back the change
         if (msg.value - price != 0) {payable(msg.sender).transfer(msg.value - price);}
+        emit Registration(msg.sender, currentOwner, name, block.timestamp + validityPeriod)
     }
 
     function refund() public {
-        payable(msg.sender).transfer(refunds[msg.sender]);
+        uint256 refundAmount = refunds[msg.sender];
+        payable(msg.sender).transfer(refundAmount);
         refunds[msg.sender] = 0;
+        emit Refund(msg.sender, refundAmount);
     }
+
     /// @param name name user wants to prolong registration for
     /// User can prolong user if the corresponding NameInfo struct still records his name, which means even in the case validity period has expired but nobody claimed the name yet
-    function prolongRegistration(string memory name) public {
+    function extendRegistration(string memory name) public {
         NameInfo storage nameInfo = names[keccak256(bytes(name))];
         require(nameInfo.owner == msg.sender, "NameSystem: the name currently doesn't belong to user.");
         nameInfo.validUntil = block.timestamp + validityPeriod;
+        emit Extension(msg.sender, name, block.timestamp + validityPeriod)
     }
+
     /// @param name name user wants to forfeit
     /// can only be called when the registration period has ended and by anyone to free up the name, but the unlocked balance will be returned to current owner
     function forfeitName(string memory name) public {
         NameInfo storage nameInfo = names[keccak256(bytes(name))];
         require(nameInfo.validUntil < block.timestamp, "NameSystem: the name hasn't expired yet");
-        payable(nameInfo.owner).transfer(namePrice(name));
+        /// again we use push and pull mechanics to prevent griefing
+        currentOwner = nameInfo.owner;
+        refunds[currentOwner] = refunds[currentOwner] + namePrice(name);
         nameInfo.owner = address(0);
         nameInfo.validUntil = 0;
+        emit Forfeiture(currentOwner, name);
     }
 }
